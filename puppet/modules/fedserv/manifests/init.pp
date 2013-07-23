0df5be3190825e1cb0141b59d7f3ca23160539c7
Nate Jones <nate@mediatemple.net>
2013-07-22 18:51:39 -0700

    disable tent.io and pump.io for now

--- puppet/modules/fedserv/manifests/init.pp
+++ puppet/modules/fedserv/manifests/init.pp
@@ -3,6 +3,6 @@ class fedserv {
   /*notify { "Setting up federated services": }*/
     
   include fedserv::mediagoblin
-  include fedserv::tentio
-  include fedserv::pumpio
+  # include fedserv::tentio
+  # include fedserv::pumpio
 }
